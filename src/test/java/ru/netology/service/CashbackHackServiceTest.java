e6c3d14ab8e839bfd552f82aa47f004501887f68
Nick <zxmadjik@mail.ru>
2021-11-19 23:21:01 +0400

    add branch junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,17 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void ShouldCalculateRemain() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int actual = service.remain(900);
-        assertEquals(expected, actual, "wrong remain");
+        assertEquals(expected, actual);
 
     }
 
@@ -21,15 +20,16 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(expected, actual, "wrong remain");
+        assertEquals(expected, actual);
 
     }
+
     @Test
     public void ShouldCalculateRemain3() {
         CashbackHackService service = new CashbackHackService();
         int expected = 1000;
         int actual = service.remain(0);
-        assertEquals(expected, actual, "wrong remain");
+        assertEquals(expected, actual);
 
     }
 }
